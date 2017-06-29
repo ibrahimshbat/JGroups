ee8e96e8bfdb6b88ebfc7e886576dcd200d97e62
a6915654 <i.s.el-sanosi@newcastle.as.uk>
2017-06-29 12:06:57 +0100

    New ZabCT version2

---
+++ src/org/jgroups/protocols/jzookeeper/zabCT_AdaptationUsingWriteRatioSyncV1/Timeout.java
@@ -0,0 +1,151 @@
+package org.jgroups.protocols.jzookeeper.zabCT_AdaptationUsingWriteRatioSyncV1;
+
+import java.io.DataInput;
+import java.io.DataOutput;
+
+import org.jgroups.util.Bits;
+import org.jgroups.util.SizeStreamable;
+import org.jgroups.util.Util;
+
+public class Timeout implements Comparable<Timeout>,  SizeStreamable {
+	
+	private MessageId id=null;
+	private long sendTime=0;
+	private long arriveTime=0;
+	private long sendBackTimeout=0;
+	private long arriveTimeForRoundTrip=0;
+	
+	
+	public Timeout() {
+
+	}
+
+	public Timeout(long sendTime) {
+		this.sendTime = sendTime;
+	}
+	
+
+//	public String getZxid() {
+//		return zxid;
+//	}
+//
+//	public void setZxid(String zxid) {
+//		this.zxid = zxid;
+//	}
+
+	public long getSendTime() {
+		return sendTime;
+	}
+
+	public void setSendTime(long sendTime) {
+		this.sendTime = sendTime;
+	}
+
+	public long getArriveTime() {
+		return arriveTime;
+	}
+
+	public void setArriveTime(long arriveTime) {
+		this.arriveTime = arriveTime;
+	}
+
+	
+	public long getSendBackTimeout() {
+		return sendBackTimeout;
+	}
+
+	public void setSendBackTimeout(long sendBackTimeout) {
+		this.sendBackTimeout = sendBackTimeout;
+	}
+
+	public long getArriveTimeForRoundTrip() {
+		return arriveTimeForRoundTrip;
+	}
+
+	public void setArriveTimeForRoundTrip(long arriveTimeForRoundTrip) {
+		this.arriveTimeForRoundTrip = arriveTimeForRoundTrip;
+	}
+	
+	
+
+	public MessageId getId() {
+		return id;
+	}
+
+	public void setId(MessageId id) {
+		this.id = id;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + (id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Timeout other = (Timeout) obj;
+		if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+	
+	@Override
+	public int compareTo(Timeout t){
+		
+		if(this.id.compareTo(t.getId())>0)
+			return 1;
+		if(this.id.compareTo(t.getId())<0)
+			return -1;		
+		else
+		return 0;
+	}
+	
+	
+
+	@Override
+	public String toString() {
+		return "Timeout [id=" + id + ", sendTime=" + sendTime + ", arriveTime=" + arriveTime + "]";
+	}
+	
+	
+	 @Override
+	    public void writeTo(DataOutput out) throws Exception {
+	        //Bits.writeString(id, out);
+	        Util.writeStreamable(id, out);
+	        Bits.writeLong(sendTime, out);
+	        Bits.writeLong(arriveTime, out);
+	        Bits.writeLong(sendBackTimeout, out);
+	        Bits.writeLong(arriveTimeForRoundTrip, out);	        
+	    }
+
+	    @Override
+	    public void readFrom(DataInput in) throws Exception {
+	        //zxid = Bits.readString(in);
+			id = (MessageId) Util.readStreamable(MessageId.class, in); 
+	        sendTime = Bits.readLong(in);
+	        arriveTime= Bits.readLong(in);
+	        sendBackTimeout= Bits.readLong(in);
+	        arriveTimeForRoundTrip=Bits.readLong(in);
+	    }
+
+		@Override
+		public int size() {
+	        return (id==null?0:id.size()) + Bits.size(sendTime) +
+	        		Bits.size(arriveTime) + Bits.size(sendBackTimeout) + Bits.size(arriveTimeForRoundTrip);
+		}
+	
+	
+	
+	
+	
+
+}
---
+++ src/org/jgroups/protocols/jzookeeper/zabCT_AdaptationUsingWriteRatioSyncV1/ZUtil.java
@@ -0,0 +1,39 @@
+package org.jgroups.protocols.jzookeeper.zabCT_AdaptationUsingWriteRatioSyncV1;
+
+import java.util.Random;
+
+import com.google.common.util.concurrent.AtomicDouble;
+
+public class ZUtil {
+
+	private AtomicDouble  p = new AtomicDouble(0.5);//0.634;//0.5;//0.6339745962155613;//1.0;//
+	static double randomValue;
+	static Random random = new Random();
+	static boolean sendAck = false;
+
+
+
+	public ZUtil(double p) {
+		this.p.set(p);
+	}
+
+
+	public boolean SendAckOrNoSend(){
+		randomValue=random.nextDouble();
+		//TO check whether to send Ack or not
+		return randomValue <= p.get()? true : false;
+
+	}
+
+	public double getP() {
+		return p.get();
+	}
+
+
+
+	public  void setP(double p) {
+		this.p.set(p);
+	}
+	
+	
+}
