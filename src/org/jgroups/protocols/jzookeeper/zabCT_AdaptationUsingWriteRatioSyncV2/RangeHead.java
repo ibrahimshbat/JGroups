6fb9a6b62ff182f504b59e4a502404998fde21dc
a6915654 <i.s.el-sanosi@newcastle.as.uk>
2017-07-26 14:31:45 +0100

    Create new package as the ZabCT alogrithm is updated.

---
+++ src/org/jgroups/protocols/jzookeeper/zabCT_AdaptationUsingWriteRatioSyncV2/RangeHead.java
@@ -0,0 +1,56 @@
+package org.jgroups.protocols.jzookeeper.zabCT_AdaptationUsingWriteRatioSyncV2;
+
+public class RangeHead {
+
+	private int alpha=1;
+	private int alphaPlusb=0;
+	private int b=0;
+	private double p=0;
+	private double WPlus1=0;
+	
+	public RangeHead() {
+
+	}
+	public RangeHead(int alpha, int alphaPlusb) {
+		this.alpha = alpha;
+		this.alphaPlusb=alphaPlusb;
+		this.b = this.alphaPlusb-this.alpha;
+	}
+	
+	public int getAlpha() {
+		return alpha;
+	}
+	public void setAlpha(int alpha) {
+		this.alpha = alpha;
+	}
+	public int getB() {
+		return b;
+	}
+	public void setB(int b) {
+		this.b = b;
+	}
+	
+	public int getAlphaPlusb() {
+		return alphaPlusb;
+	}
+	public void setAlphaPlusb(int alphaPlusb) {
+		this.alphaPlusb = alphaPlusb;
+	}
+	public double getP() {
+		return p;
+	}
+	public void setP(double p) {
+		this.p = p;
+	}
+	public double getWPlus1() {
+		return WPlus1;
+	}
+	public void setWPlus1(double wPlus1) {
+		WPlus1 = wPlus1;
+	}
+	@Override
+	public String toString() {
+		return "{"+ alpha + ", " + alphaPlusb + "}p="+p;
+	}
+	
+}
